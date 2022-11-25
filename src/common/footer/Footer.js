97e16346b40c9f4c494183be83cf786541977bb6
Lam Zhi Qiang <117005868+iamnotzq@users.noreply.github.com>
2022-11-25 16:21:23 +0800

    Add files via upload

---
+++ src/common/footer/Footer.js
@@ -0,0 +1,66 @@
+import React from "react"
+import "./Footer.css"
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid'>
+          <div className='box'>
+            <h2>ABOUT US</h2>
+            <p>We love and care for animals as if their our own. Please do follow all our social media sites to find out more about us!</p>
+            <div className='icon flex_space'>
+              <i className='fab fa-facebook-f'></i>
+              <i className='fab fa-twitter'></i>
+              <i className='fab fa-linkedin'></i>
+              <i className='fab fa-instagram'></i>
+              <i className='fab fa-pinterest'></i>
+              <i className='fab fa-youtube'></i>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>NAVIGATION</h2>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About us</Link>
+              </li>
+              
+              <li>
+                <Link to='/adopt'>Adopt</Link>
+              </li>
+              
+              <li>
+                <Link to='/release'>Release</Link>
+              </li>
+
+              <li>
+                <Link to='/contact'>Adopt Form</Link>
+              </li>
+              
+            </ul>
+          </div>
+
+          
+
+          <div className='box'>
+            <h2>NEWSLETTER</h2>
+            <p>Enter your email to receive the latest updates from us!</p>
+
+            <input type='text' name='' id='' />
+            <input type='text' className='primary-btn' value='SUBSCRIBE' />
+          </div>
+        </div>
+      </footer>
+      <div className='legal'>
+        <p>© 2022 All Rights Reserved.</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
